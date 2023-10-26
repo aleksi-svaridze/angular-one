bf9e5daabef88d38a05e6f8f5b2c0cf417190dd2
aleksi-svaridze <aleksisvaridze@outlook.com>
2023-10-26 21:28:30 +0400

    Remoe Bootstrap from project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
 import { HeroComponent } from './hero-section/hero.component';
+import { ServicesComponent } from './services/services.component';
+import { FooterComponent } from './footer/footer.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     HeroComponent,
+    ServicesComponent,
     FooterComponent
   ],
   imports: [
